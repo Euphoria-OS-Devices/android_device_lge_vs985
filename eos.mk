4d17e7cbb721ac229ea9928dd36810a02ddb1267
777jon <jcruzdeveloper@gmail.com>
2015-03-07 07:34:06 -0500

    vs985: Euphorize
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- eos.mk
+++ eos.mk
@@ -18,11 +18,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from vs985 device
-$(call inherit-product, device/lge/vs985/device.mk)
+$(call inherit-product, device/lge/vs985/vs985.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := vs985
-PRODUCT_NAME := full_vs985
+PRODUCT_NAME := eos_vs985
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-VS985
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_vzw_us" \
+    BUILD_FINGERPRINT="lge/g3_vzw/g3:5.0.1/LRX21Y/150271517d422:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_vzw-user 5.0.1 LRX21Y 150271517d422 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
